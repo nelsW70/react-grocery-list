1eae36d5e26a04fb4ae019a7d3963d059f5ef415
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-25 11:21:15 -0400

    add: add item

--- src/List.jsx
+++ src/List.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+
+const List = ({ items }) => {
+  return (
+    <div className="grocery-list">
+      {items.map(item => {
+        const { id, title } = item
+        return (
+          <article className="grocery-item" key={id}>
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn" title="edit">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn" title="delete">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        )
+      })}
+    </div>
+  )
 }
 
 export default List
